63648950d754229e7c66bddc631ce24202799e90
wr <wrong2314@163.com>
2026-01-21 21:28:19 +0800

    feat: cmd + w

--- simple_live_app/macos/Runner/AppDelegate.swift
+++ simple_live_app/macos/Runner/AppDelegate.swift
@@ -4,6 +4,19 @@ import FlutterMacOS
 @main
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return false
+  }
+
+  override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+    if !flag {
+      for window in sender.windows {
+        if (window.isVisible) {
+          continue
+        }
+        window.makeKeyAndOrderFront(self)
+        return true
+      }
+    }
     return true
   }
 
