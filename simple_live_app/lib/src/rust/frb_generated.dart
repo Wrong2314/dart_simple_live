acb6c2fcdd42f37e982e86fafbe6234d9dccf13a
wr <wrong2314@163.com>
2026-01-21 19:07:01 +0800

    ci: fix macOS build

--- simple_live_app/lib/src/rust/frb_generated.dart
+++ simple_live_app/lib/src/rust/frb_generated.dart
@@ -7,8 +7,7 @@ import 'api/danmaku_mask.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'frb_generated.dart';
-import 'frb_generated.io.dart'
-    if (dart.library.js_interop) 'frb_generated.web.dart';
+import 'frb_generated.io.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 
 /// Main entrypoint of the Rust API
